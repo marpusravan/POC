6c4f53121826787c28bb789c2da39a0e7e2fef77
vamseeuxd <vamsee.uxd@gmail.com>
2019-03-14 22:38:22 +0530

    Test

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -4,14 +4,12 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {Tab1Page} from './tab1.page';
-import {LottieAnimationViewModule} from 'ng-lottie';
 
 @NgModule({
     imports: [
         IonicModule,
         CommonModule,
         FormsModule,
-        LottieAnimationViewModule.forRoot(),
         RouterModule.forChild([{path: '', component: Tab1Page}])
     ],
     declarations: [Tab1Page]
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,43 +1,24 @@
-import {Component} from '@angular/core';
-import {NavController} from '@ionic/angular';
+import {AfterContentInit, Component, ViewChild} from '@angular/core';
+import {TweenMax, Linear} from 'gsap';
 
 @Component({
     selector: 'app-tab1',
     templateUrl: 'tab1.page.html',
     styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
-    public lottieConfig: Object;
-    private anim: any;
-    private animationSpeed = 1;
+export class Tab1Page implements AfterContentInit {
+    @ViewChild('bg') bg;
+    @ViewChild('mic') mic;
 
-    constructor() {
-        this.lottieConfig = {
-            path: 'assets/animation/lottie/mic.json',
-            renderer: 'canvas',
-            autoplay: true,
-            loop: true
-        };
-    }
-
-    handleAnimation(anim: any) {
-        this.anim = anim;
-    }
-
-    stop() {
-        this.anim.stop();
-    }
-
-    play() {
-        this.anim.play();
-    }
-
-    pause() {
-        this.anim.pause();
-    }
-
-    setSpeed(speed: number) {
-        this.animationSpeed = speed;
-        this.anim.setSpeed(speed);
+    ngAfterContentInit(): void {
+        console.log(this.bg.nativeElement);
+        console.log(this.mic.nativeElement);
+        const t1 = TweenMax.fromTo(this.bg.nativeElement, 0.5, {scale: 0}, {scale: 1, ease: Linear.easeIn, repeat: -1, repeatDelay: 0.5});
+        const t2 = TweenMax.fromTo(this.mic.nativeElement, 0.5, {bottom: -51}, {
+            bottom: 45,
+            ease: Linear.easeIn,
+            repeat: -1,
+            repeatDelay: 0.5
+        });
     }
 }
